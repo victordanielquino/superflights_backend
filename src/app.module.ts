3d075b965213205350a434b6fbbffa9558e41909
victor daniel quino jimenez <daniel@victors-MacBook-Pro.local>
2022-08-28 17:11:01 -0400

    [ADD]Conexion mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from "@nestjs/config";
+import { MongooseModule } from "@nestjs/mongoose";
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env.development'],
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.URI_MONGODB),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
