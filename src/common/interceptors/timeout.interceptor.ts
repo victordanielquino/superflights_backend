b6bcaad9f4d0672fe2f18e397dc4692b6a8c2b83
victor daniel quino jimenez <daniel@victors-MacBook-Pro.local>
2022-08-28 16:10:27 -0400

    [ADD]Exception http and interceptor global

---
+++ src/common/interceptors/timeout.interceptor.ts
@@ -0,0 +1,9 @@
+import { CallHandler, ExecutionContext, NestInterceptor } from "@nestjs/common";
+import { Observable, timeout } from "rxjs";
+
+export class TimeOutInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler<any>): Observable<any> | Promise<Observable<any>> {
+    // INTERCEPTAR PETICIONES QUE PASEN LOS 2 MINUTOS
+    return next.handle().pipe(timeout(12000));
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { AllExceptionFilter } from "./common/filters/http-exception.filter";
+import { TimeOutInterceptor } from "./common/interceptors/timeout.interceptor";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // CAPTURAMOS ERROR HTTP DE MANERA GLOBAL
+  app.useGlobalFilters(new AllExceptionFilter());
+  // INTERCEPTAMOS LAS PETICIONES QUE PASEN DE LOS 2 MINUTOS DE MANERA GLOBAL
+  app.useGlobalInterceptors(new TimeOutInterceptor());
   await app.listen(3000);
 }
 bootstrap();
